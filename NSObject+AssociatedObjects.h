bad56af095b807f7cc332ddf8d4fd7e5a9af1d75
Ruslan Kavetsky <rusya.182@gmail.com>
2016-12-20 16:55:40 +0300

    Add defines for ivar generation

--- NSObject+AssociatedObjects.h
+++ NSObject+AssociatedObjects.h
@@ -9,7 +9,35 @@
 
 @interface NSObject (AssociatedObjects)
 
-#define AssociatedObjectKey(key) @selector(key)
+#define AssociatedObjects_Key(key) @selector(key)
+
+#define AssociatedObjects_generateObjectGetter(type, getter)\
+- (type)getter {                                          \
+    return [self associatedObjectForKey:AssociatedObjects_Key(getter)]; \
+}                                                           \
+
+#define AssociatedObjects_generateObjectSetter(type, getter, setter)\
+- (void)setter:(type)v {                                  \
+    [self setAssociatedObject:v forKey:AssociatedObjects_Key(getter)];  \
+}                                                           \
+
+#define AssociatedObjects_generatePrimitiveGetter(type, getter, unwrapMethod)\
+- (type)getter {                                                    \
+    return [[self associatedObjectForKey:AssociatedObjects_Key(getter)] unwrapMethod];  \
+}                                                                   \
+
+#define AssociatedObjects_generatePrimitiveSetter(type, getter, setter)\
+- (void)setter:(type)v {                                        \
+    [self setAssociatedObject:@(v) forKey:AssociatedObjects_Key(getter)];   \
+}                                                               \
+
+#define AssociatedObjects_generateForObject(type, getter, setter)\
+AssociatedObjects_generateObjectGetter(type, getter)\
+AssociatedObjects_generateObjectSetter(type, getter, setter)\
+
+#define AssociatedObjects_generateForPrimitive(type, getter, setter, unwrapMethod)\
+AssociatedObjects_generatePrimitiveGetter(type, getter, unwrapMethod)\
+AssociatedObjects_generatePrimitiveSetter(type, getter, setter)
 
 - (void)setAssociatedObject:(id)object forKey:(void *)key;
 - (void)setAssociatedWeekObject:(id)object forKey:(void *)key;
